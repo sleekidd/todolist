b56b634a202847e7b17c6347fee1b7455993b922
thetobiallen <tobi_newsystem@yahoo.com>
2020-10-12 08:48:29 +0100

    First commit

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    text = models.CharField(max_length=120)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
